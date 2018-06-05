e91cf96656fea36be06764ae127cd9816b496d7e
Anusha Konatala <anu.konatala@gmail.com>
2018-06-05 21:19:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  name_hash = {'first_name': 'Anusha', 'last_name': 'Konatala'}
+  name_hash
   
 end
 
